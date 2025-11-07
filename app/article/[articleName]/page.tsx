bc27ea4a3d18aedd8b931a4fce6c4da6869e466d
hriday-patel <patelhriday23@gmail.com>
2025-11-07 18:24:12 +0530

    update

--- app/article/[articleName]/page.tsx
+++ app/article/[articleName]/page.tsx
@@ -1,5 +1,8 @@
-import Link from "next/link";
+"use client";
 
+import Link from "next/link";
+import { use } from "react";
+import { useRouter } from "next/navigation";
 type Params = {
   params: Promise<{ articleName: string }>;
 };
@@ -8,9 +11,11 @@ type SearchParams = {
   searchParams: Promise<{ lang?: "es" | "fr" | "de" | "en" }>;
 };
 
-const page = async ({ params, searchParams }: Params & SearchParams) => {
-  const { articleName } = await params;
-  const { lang = "en"} = await searchParams;
+const page = ({ params, searchParams }: Params & SearchParams) => {
+  const { articleName } = use(params);
+  const { lang = "en"} = use(searchParams);
+  const router = useRouter();
+  console.log("Router object:", router);
   return (
     <div>
       <div>This is article on about {articleName}</div>
