b828fe8d0e9671bd57f8140da4952f3b38d8853e
hriday-patel <patelhriday23@gmail.com>
2025-11-15 02:02:19 +0530

    update

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,7 +1,13 @@
+import { log } from "console";
+import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 
 export async function GET(request: NextRequest){
-    console.log(request.headers.get("Authorization"))
+    // console.log(request.headers.get("Authorization"))
+    const header = (await headers()).get("Authorization");
+    log(header);
     return Response.json("Hello !");
 }
+
+
