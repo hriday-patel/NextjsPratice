84843c13441d552154c7de3a6d98a1e56447bf47
hriday-patel <patelhriday23@gmail.com>
2025-11-20 10:49:49 +0530

    update

--- app/productApi/route.ts
+++ app/productApi/route.ts
@@ -1,13 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-
+import fs from "fs";
+import path from "path";
 
 import { addProduct } from "@/app/prisma-db";
-import Products from '../../products.json';
+import Products from "../../products.json";
+
 export async function POST(request: Request) {
   const body = await request.json();
   const { name, price, description } = body;
-  Products.push({name, price: parseInt(price), description});
-  fs.writeFileSync(path.join('products.json'), JSON.stringify(Products));
+  Products.push({ name, price: parseInt(price), description });
+  fs.writeFileSync(path.join("products.json"), JSON.stringify(Products));
   return Response.json(Products);
 }
